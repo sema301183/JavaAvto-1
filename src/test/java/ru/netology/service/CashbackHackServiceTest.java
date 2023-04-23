99460b214efea8d399dddcae92f6db8688d70bcc
Sema test <ganzalovdmitriy@gmail.com>
2023-04-23 22:03:32 +0300

    .gitignore

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,7 +1,37 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
+import org.junit.jupiter.api.Test;
 
-public class CashbackHackServiceTest {
+import static org.junit.jupiter.api.Assertions.*;
 
-}
+class CashbackHackServiceTest {
+
+    @Test
+    void shouldReturn100IfAmountIs900() {
+        // подготовка данных
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int expected = 100;
+
+        // выполнение целевого действия
+        int actual = service.remain(amount);
+
+        // сравнение ОР и ФР
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn0IfAmountIs1000() {
+        // подготовка данных
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+
+        // выполнение целевого действия
+        int actual = service.remain(amount);
+
+        // сравнение ОР и ФР
+        assertEquals(expected, actual);
+    }
+
+}gi
